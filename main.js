362543ef91e9bcb17ebe6b9450a350ed16190a72
Matthew Reichardt <me@matthewreichardt.com>
2021-07-04 22:12:08 -0500

    Fixed formatting,  made dynamic, added @zanozon

---
+++ main.js
@@ -0,0 +1,24 @@
+var app = new Vue({
+    el: '#app',
+
+    // Boiler listing template: {alias: '@', description: '', contactPrefs: {aliasOnly: false, discord: '', github: '', email: '', telegram: ''}}
+
+    data: {
+        listings: {
+            development: [
+                {alias: '@theagiledev123', description: 'Full-stack web developer with special interests in decentralized applications, authentication, and geo-spatial applications.', contactPrefs: {aliasOnly: false, discord: 'theagiledev#8936', github: 'https://github.com/matthewjamesr', email: '', telegram: 'theagiledev'}}
+            ],
+            art_design: [],
+            finance: [],
+            services: [
+                {alias: '@zanozon', description: 'We help our clients procure Books and other miscellaneous items.', contactPrefs: {aliasOnly: true, discord: '', github: '', email: '', telegram: ''}}
+            ]
+        }
+    },
+    methods: {
+        // Methods
+    },
+    mounted: function () {
+        // Do things when app loads
+    }
+})
